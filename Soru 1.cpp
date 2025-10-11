9da4ff63a7cdf0f1e278f4c8e3f8010d848dcd0a
enezsabbah <m.uzunyuz@mf.karaelmas.edu.tr>
2025-10-11 14:45:29 +0300

    Add files via upload
    İki sayının toplamı

---
+++ Soru 1.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	
+	int a,b,c;
+	printf("1.sayiyi giriniz:");
+	scanf("%d",&a);
+	printf("2.sayiyi giriniz:");
+	scanf("%d",&b);
+	c=a+b;
+	printf("Toplama islemi sonucu:%d",c);
+		
+	  
+    return 0;
+}
